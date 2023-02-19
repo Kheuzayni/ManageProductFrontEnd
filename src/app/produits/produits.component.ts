8e68ea284b6f90713bce1be71cfdceab644c3dc0
unknown <bayecheikhfall249@gmail.com>
2023-02-19 19:33:01 +0000

    Databinding liste produit

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
 
 @Component({
   selector: 'app-produits',
@@ -7,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProduitsComponent implements OnInit{
 
-  produits : string[] | undefined;
+  //produits : string[] | undefined; //Tableau de chaine de caractère
+
+  produits : Produit[] | undefined; //tableau de produit
 
   constructor (){
-    this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
+    //this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
+    
+    this.produits = [
+      {idProduit : 1, nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011")},
+      {idProduit : 2, nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010")},
+      {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020")}
+    ]
   }
 
   ngOnInit():void {}
