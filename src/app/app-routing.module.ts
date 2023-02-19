865990a982b498acc2d7b82bddad0128aae7dc8e
unknown <bayecheikhfall249@gmail.com>
2023-02-19 16:41:46 +0000

    add products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProduitComponent } from './add-produit/add-produit.component';
 import { ProduitsComponent } from './produits/produits.component';
 
 const routes: Routes = [
-  {path: 'produits', component : ProduitsComponent}
+  {path: 'produits', component : ProduitsComponent},
+  {path: 'addproduits', component : AddProduitComponent}
 ];
 
 @NgModule({
