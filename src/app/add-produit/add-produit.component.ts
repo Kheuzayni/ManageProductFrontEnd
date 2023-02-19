c4dd648143ae36cb7ecea84bf0bda3a0cb29f40d
unknown <bayecheikhfall249@gmail.com>
2023-02-19 22:13:16 +0000

    Form add product

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Produit } from '../model/produit.model';
 
 @Component({
   selector: 'app-add-produit',
   templateUrl: './add-produit.component.html',
   styleUrls: ['./add-produit.component.css']
 })
-export class AddProduitComponent {
+export class AddProduitComponent implements OnInit{
+
+  constructor(){}
+
+  newProduit = new Produit();
+
+  addProduit(){ console.log(this.newProduit); }
+
+  ngOnInit(): void {
+    
+  }
 
 }
