334eae0c6d813811ee348f4fcceba964a97e2bf7
unknown <bayecheikhfall249@gmail.com>
2023-02-20 19:46:25 +0000

    message : fix udefined

--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Produit } from '../model/produit.model';
+import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-produits',
@@ -12,15 +13,18 @@ export class ProduitsComponent implements OnInit{
 
   produits : Produit[] | undefined; //tableau de produit
 
-  constructor (){
+  constructor (private produitService: ProduitService){
     //this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
     
-    this.produits = [
-      {idProduit : 1, nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011")},
-      {idProduit : 2, nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010")},
-      {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020")}
-    ]
-  }
+  //   this.produits = [
+  //     {idProduit : 1, nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011")},
+  //     {idProduit : 2, nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010")},
+  //     {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020")}
+  //   ]
+
+  this.produits = produitService.listeProduits();
+  
+   }
 
   ngOnInit():void {}
 }
