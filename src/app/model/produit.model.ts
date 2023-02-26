1d542eca4a342beed75031fd36d73cbbb3506512
unknown <bayecheikhfall249@gmail.com>
2023-02-26 21:58:02 +0000

    add categorie dans produitcategorie! : Categorie;model

--- src/app/model/produit.model.ts
+++ src/app/model/produit.model.ts
@@ -1,6 +1,9 @@
+import { Categorie } from "./categorie.module";
+
 export class Produit {
-    idProduit? : number;
-    nomProduit? : string;
-    prixProduit? : number;
-    dateCreation? : Date ; 
+    idProduit! : number;
+    nomProduit! : string;
+    prixProduit! : number;
+    dateCreation! : Date ;
+    categorie! : Categorie; 
 }
