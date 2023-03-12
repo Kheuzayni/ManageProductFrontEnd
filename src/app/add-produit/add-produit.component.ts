a974664d7176a4954affc3f3c77093c5d408cc10
unknown <bayecheikhfall249@gmail.com>
2023-03-12 20:02:44 +0000

    Fix error

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,70 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Categorie } from '../model/categorie.model';
 import { Produit } from '../model/produit.model';
 import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-add-produit',
-  templateUrl: './add-produit.component.html',
-  styleUrls: ['./add-produit.component.css']
+  templateUrl: './add-produit.component.html'
 })
-export class AddProduitComponent implements OnInit{
+export class AddProduitComponent implements OnInit {
 
-  message ?: string;
   newProduit = new Produit();
-
-  categories! : Categorie[]; 
-  newIdCat! : number; 
+  categories! : Categorie[];
+  newIdCat! : number;
   newCategorie! : Categorie;
-  router: any;
   
-  constructor(private produitService: ProduitService) { }
+  constructor(private produitService: ProduitService,
+              private router : Router) { }
 
-  ngOnInit(): void { 
-    this.produitService.listeCategories(). 
-    subscribe(cats => {console.log(cats); 
-      this.categories = cats._embedded.categories; 
-    } ); 
+  ngOnInit(): void {
+
+    this.produitService.listeCategories().
+          subscribe(cats => {this.categories = cats._embedded.categories;
+            console.log(cats);
+        });
+ 
   }
 
-  // ngOnInit(): void { this.produitService.listeCategories().
-  //    subscribe(cats => {
-  //     this.categories = cats; 
-  //     console.log(cats); }); 
-  //   }
-  // ngOnInit(): void {this.categories = this.produitService.listeCategories();}
-
-  // addProduit(){
-  //   this.newCategorie = this.produitService.consulterCategorie(this.newIdCat); 
-  //   this.newProduit.categorie = this.newCategorie; 
-  //   this.produitService.ajouterProduit(this.newProduit); 
-  //   this.router.navigate(['produits']);
-    
-  //   console.log(this.newProduit); 
-  //   console.table(this.newProduit);
-    
-  //   this.produitService.ajouterProduit(this.newProduit);
-
-  //   this.message = "Produit "+ this.newProduit.nomProduit +" ajouté avec succes";
-  // }
-
-  // addProduit(){ 
-  //   this.produitService.ajouterProduit(this.newProduit) 
-  //     .subscribe(prod => { 
-  //       console.log(prod); 
-  //       this.router.navigate(['produits']); 
-  //     }
-  //   ); 
-  //   }
-
+ 
   addProduit(){
-     this.newProduit.categorie = this.categories.find(cat => cat.idCat == this.newIdCat)!; 
-     this.produitService.ajouterProduit(this.newProduit) .subscribe(prod => { 
-      console.log(prod); 
-      this.router.navigate(['produits']); 
-    }); 
-  }
+    this.newProduit.categorie = this.categories.find(cat => cat.idCat == this.newIdCat)!;
+    this.produitService.ajouterProduit(this.newProduit)
+                      .subscribe(prod => {
+                      console.log(prod);
+                      this.router.navigate(['produits']);
+                      }); 
+    }
 
-  
+
+
 
 }
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -4,58 +4,39 @@ import { ProduitService } from '../services/produit.service';
 
 @Component({
   selector: 'app-produits',
-  templateUrl: './produits.component.html',
-  styleUrls: ['./produits.component.css']
+  templateUrl: './produits.component.html'
 })
-export class ProduitsComponent implements OnInit{
+export class ProduitsComponent implements OnInit {
 
-  //produits : string[] | undefined; //Tableau de chaine de caractère
+    produits? : Produit[]; //un tableau de produits
 
-  produits ?: Produit[]; //tableau de produit
+  constructor(private produitService: ProduitService) {
+   //this.produits=[];
+     }
 
-  constructor (private produitService: ProduitService){
-    //this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
-    
-  //   this.produits = [
-  //     {idProduit : 1, nomProduit : "PC Asus", prixProduit : 3000.600, dateCreation : new Date("01/14/2011")},
-  //     {idProduit : 2, nomProduit : "Imprimante Epson", prixProduit : 450, dateCreation : new Date("12/17/2010")},
-  //     {idProduit : 3, nomProduit :"Tablette Samsung", prixProduit : 900.123, dateCreation : new Date("02/20/2020")}
-  //   ]
+  ngOnInit(): void {
 
-  this.produits = produitService.listeProduits();
-  
-   }
-
-  // ngOnInit():void {
-  //   this.produitService.listeProduit().subscribe(prods => { 
-  //     console.log(prods); 
-  //     this.produits = prods;
-  // }
-
-  // supprimerProduit(produit : Produit) {
-  //   //console.log(p);
-    
-  //   let conf = confirm("Etes-vous sûr ?");
-  //   if (conf) 
-  //   this.produitService.supprimerProduit(produit);
-
-  // }
-
-  ngOnInit(): void { 
-    this.chargerProduits(); 
-  } 
-  
-  chargerProduits(){ 
-    this.produitService.listeProduit()
-    .subscribe(prods => { console.log(prods); this.produits = prods; }); 
-  } 
-  
-  supprimerProduit(p: Produit) { 
-    let conf = confirm("Etes-vous sûr ?");
-     if (conf) this.produitService.supprimerProduit(p.idProduit).subscribe(() => { 
-      console.log("produit supprimé"); 
-      this.chargerProduits(); }); 
+    this.chargerProduits();
   }
 
-}
+  chargerProduits(){
+    this.produitService.listeProduit().subscribe(prods => {
+      console.log(prods);
+      this.produits = prods;
+      });
+  }
 
+supprimerProduit(p: Produit)
+{
+let conf = confirm("Etes-vous sûr ?");
+if (conf)
+  this.produitService.supprimerProduit(p.idProduit).subscribe(() => {
+        console.log("produit supprimé");
+        this.chargerProduits();     
+      
+});
+}
+ 
+ 
+
+}
